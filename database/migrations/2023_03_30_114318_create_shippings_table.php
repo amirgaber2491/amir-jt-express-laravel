d8f35e4ff6a70c1e7677b8199a243299cf38fbae
Amir Gaber <ameergaber6@gmail.com>
2024-06-20 22:13:30 +0300

    CreateShippingJTExpressTable

--- database/migrations/2023_03_30_114318_create_shippings_table.php
+++ database/migrations/2023_03_30_114318_create_shippings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateShippingTable extends Migration
+class CreateShippingJTExpressTable extends Migration
 {
     /**
      * Run the migrations.
